ffec5bfdc99c333799df4a0a6b1808d88eb113ef
ref2ct <Felipe.Reimer@br.bosch.com>
2024-12-03 11:36:01 -0300

    Minor fixes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import authRoutes from './routes/auth';
 import voteRoutes from './routes/vote';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/auth', authRoutes);
 app.use('/vote', voteRoutes);
@@ -12,3 +14,8 @@ app.use('/vote', voteRoutes);
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
 });
+
+app.get('/test', (req, res) => {
+  res.send('Server is running');
+});
+
